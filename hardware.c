d4f2a50e0d7b32c76e87fe23e368853bf34f663b
Robson <robsoncouto@outlook.com>
2017-02-03 15:40:25 -0300

    Now a push button can be connected between 1 and 3 in the ZIF socket. The button on the board makes the LCD backlight turn off while is pressed

--- hardware.c
+++ hardware.c
@@ -25,10 +25,17 @@ void init_hardware(void){
 
   ADMUX = (1<<REFS0)|(1<<MUX1)|(1<<MUX0);//PIN ADC7 used (ADC exclusive)
   ADCSRA = (1<<ADEN)|(1<<ADPS2)|(1<<ADPS0);
-  //button
+  //"native" button
   DDRD&=~(1<<7);
-  //PORTD=(1<<7);
+  //push button connected between terminal 1 and 3 of the ZIF socket
+  DDRB=~(1<<PINB4);//Input
+  DDRB|=(1<<PINB5)|(1<<PINB0); //PB5 is 5V and PB0 is 0V
+  PORTB=(1<<5);
+  PORTB&=~(1<<0);
+
   init_timer();
+
+
 }
 uint16_t get_adc(uint8_t channel){
   ADMUX&=0xF0;
@@ -44,7 +51,10 @@ uint8_t getrand(uint8_t max){
   return randNumber;
 }
 uint8_t buttonIsPressed(void){
-  return !(PIND&(1<<7));
+  uint8_t press=0;
+  press=!(PINB&(1<<4));
+  press|=!(PIND&(1<<7));
+  return press;
 }
 
 
